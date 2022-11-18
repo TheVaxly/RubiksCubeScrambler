d51c6abd571496414f5cfd83d5b8efc42d815ddf
ThomasTheFknTrainEngine <114987279+TheVaxly@users.noreply.github.com>
2022-11-18 16:17:18 +0100

    Add files via upload

---
+++ RubiksCubeScrambler.py
@@ -0,0 +1,62 @@
+import random
+import time
+
+yes = input(' Kas soovite genereerida 3x3 või 2x2 scramble? (3/2): ')
+moves = ["U", "D", "F", "B", "R", "L", "U'", "D'", "F'", "B'", "R'", "L'", "U2", "D2", "F2", "B2", "R2", "L2"]
+
+def three_by_three():
+    s = [random.choice(moves) for x in range(random.randint(20, 23))]
+    return ' '.join(s)
+
+def two_by_two():
+    s = [random.choice(moves) for x in range(random.randint(9, 12))]
+    return ' '.join(s)
+
+kriips2 = '---------------------------------\n'
+kriips = '\n------------------------------'
+kriips3 = '\n---------------'
+
+a = three_by_three()
+b = two_by_two()
+
+if yes == '3':
+    print(kriips2 + ' 3x3 Scramble: ' + a)
+elif yes == '2':
+    print(kriips2 + ' 2x2 Scramble: ' + b)
+
+def aeg(seK):
+    min = seK // 60
+    seK = seK % 60
+    min = min % 60
+    print(kriips2 + " Teie aeg oli: {0}:{1}".format(int(min),seK) + kriips3)
+    if lõpp_aeg >= 30:
+        print(' Ur trash kid!')
+    elif lõpp_aeg <= 10:
+        print(' Holy cow!')
+    else:
+        print(' Noice!')
+
+def aeg2(seK):
+    min = seK // 60
+    seK = seK % 60
+    min = min % 60
+    print(kriips2 + " Teie aeg oli: {0}:{1}".format(int(min),seK) + kriips3)
+    if lõpp_aeg >= 20:
+        print(' Ur trash kid!')
+    elif lõpp_aeg <= 5:
+        print(' Holy cow!')
+    else:
+        print(' Noice!')
+
+input(f"{kriips2} Vajutage ENTER, et alustada aeg")
+algus = time.time()
+input(f"{kriips2} Vajutage uuesti ENTER, et lõpetada aeg")
+lõpp = time.time()
+
+lõpp_aeg0 = lõpp - algus
+lõpp_aeg = round(lõpp_aeg0, 3)
+
+if yes == '3':
+    aeg(lõpp_aeg)
+elif yes == '2':
+    aeg2(lõpp_aeg)
